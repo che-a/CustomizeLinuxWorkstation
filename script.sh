bfe7389788852da892442a74dc0fc8c28eb9a096
che-a <tech.dep.it@ya.ru>
2021-06-13 13:23:37 +0300

    Добавлена установка необходимых пакетов

--- script.sh
+++ script.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+
+apt-get update && apt-get upgrade -y
+
+apt-get install -y \
+    curl \
+    fonts-ubuntu \
+    htop \
+    lm-sensors \
+    mc \
+    remmina \
+    screenfetch \
+    tmux tmux-themepack-jimeh \
+    tree \
+    vlc
+
+# Установка Atom
+
+
+# Установка Dbeaver
